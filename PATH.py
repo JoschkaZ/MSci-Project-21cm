726b0126fcb6a5c16b39fc5ff719581c620f30e4
KatoriHirokazu <HirokazuKatori@dyn3158-60.wlan.ic.ac.uk>
2018-10-30 14:06:22 +0000

    retyped my path

--- PATH.py
+++ PATH.py
@@ -2,8 +2,7 @@ from sys import platform
 
 def get_path():
     if platform == "darwin":
-        #return r'/Users/HirokazuKatori/Desktop/MSci Project'⁨ ⁨
-        1
+        return r'/Users/HirokazuKatori/Desktop/Msci Project'
     elif platform == "linux":
         return r'/home/jz8415/21cmFAST-master'
     else:
